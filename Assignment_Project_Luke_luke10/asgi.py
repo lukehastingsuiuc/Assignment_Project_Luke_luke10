e10bf90ffcffefedc618136e24e8557313292cf3
Luke Hastings <luke10@illinois.edu>
2025-09-11 17:15:40 -0500

    django setup

---
+++ Assignment_Project_Luke_luke10/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Assignment_Project_Luke_luke10 project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Assignment_Project_Luke_luke10.settings')
+
+application = get_asgi_application()
