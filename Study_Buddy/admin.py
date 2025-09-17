1c8456eb90d1b3e5134cb7115114f4d2b0a13d11
Luke Hastings <luke10@illinois.edu>
2025-09-17 13:47:37 -0500

    Created superuser and registered models

--- Study_Buddy/admin.py
+++ Study_Buddy/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import User, Materials, Assignment
+admin.site.register(Materials)
+admin.site.register(Assignment)
+admin.site.register(User)
