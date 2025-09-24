88af458fc8386d293fce1b81dccb426641198a61
Luke Hastings <luke10@illinois.edu>
2025-09-24 15:04:28 -0500

    added detail view for viewing user details including associated email and assignments

--- Study_Buddy/models.py
+++ Study_Buddy/models.py
@@ -6,17 +6,19 @@ class User(models.Model):
     user_id = models.AutoField(primary_key=True)
     password = models.CharField(max_length=15)
     email = models.EmailField(unique=True)
+    def __str__(self):
+        return f'{self.user_id}'
 
 class Assignment(models.Model):
     asn_id = models.AutoField(primary_key=True)
     due_date = models.DateField()
     past_due = models.BooleanField(default=False)
     due_soon = models.BooleanField(default=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='assignments_related_name')
     class Meta:
         ordering = ['due_date']
 
 class Materials(models.Model):
     mat_id = models.AutoField(primary_key=True)
     type = models.CharField(max_length=20)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    assignments_related_name = models.ForeignKey(User, on_delete=models.CASCADE)
--- Study_Buddy/views.py
+++ Study_Buddy/views.py
@@ -1,18 +1,37 @@
 
 from django.http import HttpResponse
 from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+from django.views import View
 from .models import User
 # Create your views here.
 
 def user_list_view(request):
     users = User.objects.all()
-    template = loader.get_template("users/user_list.html")
     context = {"users": users}
-    output = template.render(context, request)
+    template = loader.get_template("users/user_list.html")
+    output = template.render(context)
     return HttpResponse(output)
 
 def user_list_render(request):
     users = User.objects.all()
     context = {"user_list_html": users}
     return render(request, "users/user_list.html", context)
+
+class UserListView(ListView):
+    model = User
+    context_object_name = "user_rows_for_looping"
+
+class UserDetail(View):
+    def get(self, request, primary_key):
+        user = get_object_or_404(User, pk=primary_key)
+        assignments = user.assignments_related_name.all()
+        return render(
+            request,
+            'users/user_detail.html',
+            {
+                'single_user_var_for_looping': user,
+                'assignments_var_for_looping': assignments,
+            },
+        )
